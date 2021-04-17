f08db0483da10629481593585377bd139266f2a5
Stendart <Reiden1998@yandex.ua>
2021-04-17 20:03:48 +0300

    add fonts

--- webpack/webpack.base.conf.js
+++ webpack/webpack.base.conf.js
@@ -60,7 +60,7 @@ module.exports = {
       },
       {
         // Fonts
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|otf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'file-loader',
         options: {
           name: '[name].[ext]'
