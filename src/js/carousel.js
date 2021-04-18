82c539f48ca73099cd82a45637aeecfbeb8c25ed
Stendart <Reiden1998@yandex.ua>
2021-04-18 12:17:12 +0300

    add tablet carousel

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -81,32 +81,39 @@ const items = document.querySelectorAll('.js-item');
 const prevBtn = document.querySelector('.js__prev');
 const nextBtn = document.querySelector('.js__next');
 
-let count = 0;
+const carouselPoitns = document.querySelector('.carousel__poitns');
+const sliderPoints = [];
+console.log('carouselPoitns', carouselPoitns)
+
+let numSlide = 0;
 
 
 function changeSlide(direction) {
   if(direction === 'next') {
-    if(count >= items.length-1) {
-      count = 0;
+    if(numSlide >= items.length-1) {
+      numSlide = 0;
     } else {
-      count++;
+      numSlide++;
     }
   } else if (direction === 'prev') {
-    if(count <= 0) {
-      count = items.length-1;
+    if(numSlide <= 0) {
+      numSlide = items.length-1;
     } else {
-      count--;
+      numSlide--;
     }
   }
+
+  sliderPoints.forEach(item => {
+    item.classList.remove('carousel__item-point--select');
+  });
+  sliderPoints[numSlide].classList.add('carousel__item-point--select');
 
   setSlide();
 }
 
-// изменять transition: transform 1s; прям в JS. Перед удалением класса ставить 0, а перед повлением - 1
-
 function setSlide() {
   items.forEach((item, index) => {
-    if(index === count) {
+    if(index === numSlide) {
       item.style.zIndex = 2;
       item.classList.add('item__fast-anim');
       item.classList.remove('hide');
@@ -115,11 +122,28 @@ function setSlide() {
       item.classList.add('item__low-anim');
       item.classList.remove('item__fast-anim');
       item.classList.add('hide');
-
     }
 
   });
 }
+
+function createDomElement(el = 'li') {
+  const newEl = document.createElement(el);
+  newEl.classList.add('carousel__item-point');
+
+  return newEl;
+}
+
+items.forEach(item => {
+  sliderPoints.push(createDomElement('li'));
+});
+
+sliderPoints[numSlide].classList.add('carousel__item-point--select');
+
+sliderPoints.forEach(item => {
+    carouselPoitns.appendChild(item);
+});
+
 setSlide();
 prevBtn.addEventListener('click', () => changeSlide('prev'));
 nextBtn.addEventListener('click', () => changeSlide('next'));
